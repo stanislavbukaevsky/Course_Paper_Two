75e196faa5d599f497307385de736a80617713ec
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-11-02 13:30:38 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/coursepapertwo/CoursePaperTwoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursepapertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaperTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaperTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/coursepapertwo/CoursePaperTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursepapertwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePaperTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
